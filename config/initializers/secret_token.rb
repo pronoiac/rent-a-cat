71d6b0278a7d2a70fbc2ab3394e8da7565030043
App Academy Student <student@appacademy.io>
2014-08-26 10:29:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RentACat::Application.config.secret_key_base = 'be1e527838b7fdba4d5664aff5c72906d715bcd8f9d58aa169b8540df2ded6da0b5c52db90e3568b850242add555582c8025f61a01c9b4136b33e347f5c3194c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RentACat::Application.config.session_store :cookie_store, key: '_rent-a-cat_session'
