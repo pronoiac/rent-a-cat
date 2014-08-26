8ff2f0df27e1fe9c27c1b33feb992895730cee76
App Academy Student <student@appacademy.io>
2014-08-26 14:38:20 -0700

    Cat creation with cat/new

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -11,12 +11,25 @@ class CatsController < ApplicationController
   
   def new
     # @cat = Cat.new()
+    
     render :new
+  end
+  
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      render :show
+    else
+      render(
+        json: @cat.errors.full_messages, status: :unprocessable_entity
+      )
+    end
   end
   
   private
   def cat_params
-    params.require(:cat).permit(*params)
+    params.require(:cat).permit(:name, :age, :color, :birth_date, :sex, :description)
+    # params.require(:cat).permit(*params)
   end
   
 end
