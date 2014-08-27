2b6f2eb809e1a128714172f7538bec74783c5a8a
App Academy Student <student@appacademy.io>
2014-08-27 16:37:12 -0700

    On failed login, keep entered values.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,17 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:user_name],
       params[:user][:password]
     )
 
-    if user.nil?
+    if @user.nil?
+      @user = User.new(user_params)
       flash.now[:errors] ||= [] << "Credentials were wrong"
       render :new
     else
-      session[:session_token] = user.session_token
+      session[:session_token] = @user.session_token
       flash[:notices] ||= [] << "Successfully logged in!"
       redirect_to cats_url
     end
@@ -33,4 +34,9 @@ class SessionsController < ApplicationController
     redirect_to cats_url if logged_in? 
   end
   
+  private
+  
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
 end
